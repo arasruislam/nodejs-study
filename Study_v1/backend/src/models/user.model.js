70f82569249b737e096ebb5a68e102c33c381073
arasruislam <arasru01@gmail.com>
2025-01-28 08:37:12 +0600

    user and video schema model

---
+++ Study_v1/backend/src/models/user.model.js
@@ -0,0 +1,53 @@
+// External Import
+import mongoose, { Schema } from "mongoose";
+
+// Schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistroy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Export Schema
+export const User = mongoose.model("User", userSchema);
