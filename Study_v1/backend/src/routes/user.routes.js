7fddb5230e92097cbfd6c5dda662f092fc0912d7
arasruislam <arasru01@gmail.com>
2025-01-29 16:51:25 +0600

    add register controller

--- Study_v1/backend/src/routes/user.routes.js
+++ Study_v1/backend/src/routes/user.routes.js
@@ -3,12 +3,25 @@ import { Router } from "express";
 
 // Internal Import
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 // Create Router
 const router = Router();
 
 // route
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 // Export Router
 export default router;
