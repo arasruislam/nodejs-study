4055dcd586d433a933d61357b4901f4d40895cc9
arasruislam <arasru01@gmail.com>
2025-06-29 05:33:13 +0600

    learn fs

---
+++ fundamentals/fs.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+console.log("task 1");
+
+fs.writeFileSync("./task.txt", "learning file system and got error");
+
+const data = fs.readFileSync("./task.txt", { encoding: "utf-8" });
+
+console.log(data)
