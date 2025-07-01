0255259686975631a3007eba2eeb54d8077aa104
arasruislam <arasru01@gmail.com>
2025-07-02 04:57:05 +0600

    ...

--- fundamentals/fileRead.js
+++ fundamentals/fileRead.js
@@ -1,22 +1,43 @@
-const { error } = require("console");
+// const { error } = require("console");
+// const fs = require("fs");
+
+// console.log("task 1");
+
+// const data = fs.readFile("./task.txt", { encoding: "utf-8" }, (err, data) => {
+//   if (err) {
+//     console.log("something went wrong! ", err);
+//     return;
+//   }
+
+//   fs.writeFile("./task.txt", data, { encoding: "utf-8" }, (err) => {
+//     if (err) {
+//       console.log("something went wrong!!!", err);
+//       return;
+//     }
+
+//     console.log("Written successfully!!!!");
+//   });
+// });
+
+// console.log("Task 2");
+
 const fs = require("fs");
 
-console.log("task 1");
+const readStream = fs.createReadStream("./task.txt", { encoding: "utf-8" });
+const writeStream = fs.createWriteStream("./task.txt", { encoding: "utf-8" });
 
-const data = fs.readFile("./task.txt", { encoding: "utf-8" }, (err, data) => {
-  if (err) {
-    console.log("something went wrong! ", err);
-    return;
-  }
+readStream.on("data", (data) => {
+  console.log(data);
 
-  fs.writeFile("./task.txt", data, { encoding: "utf-8" }, (err) => {
+  writeStream.write(data, (err) => {
     if (err) {
-      console.log("something went wrong!!!", err);
-      return;
+      throw Error("Error", err);
     }
-
-    console.log("Written successfully!!!!");
   });
 });
 
-console.log("Task 2");
+readStream.on("error", (err) => {
+  if (err) {
+    throw Error("Error", err);
+  }
+})
