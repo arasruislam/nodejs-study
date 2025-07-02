a88d1cd68100a2f1dca47b3792924757137f2d69
arasruislam <arasru01@gmail.com>
2025-07-02 22:43:56 +0600

    learn route

--- fundamentals/todo-app/server.js
+++ fundamentals/todo-app/server.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req, res);
-  res.end("Welcome to todo app server");
+  console.log(req.url, req.method);
+  // res.end("Welcome to todo app server");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todo");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todo created.");
+  } else {
+    res.end("route not found!!!");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
