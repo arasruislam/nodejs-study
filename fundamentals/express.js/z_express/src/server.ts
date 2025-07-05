252c9664bd8572b0f9da4d90fdeaaeebb70f8020
arasruislam <arasru01@gmail.com>
2025-07-06 02:31:12 +0600

    ...

--- fundamentals/express.js/z_express/src/server.ts
+++ fundamentals/express.js/z_express/src/server.ts
@@ -1,9 +1,28 @@
+import { Collection, MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 
 let server;
 const port = 5000;
 
+const uri =
+  "mongodb+srv://ashraful:ScaleUp17226@cluster0.qmhauay.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
 const bootstrap = async () => {
+  await client.connect();
+  console.log("Connected To MongoDB")
+
+  const db = await client.db("todosDB").collection("todos");
+
   server = app.listen(port, () => {
     console.log(`The server running on port ${port}`);
   });
