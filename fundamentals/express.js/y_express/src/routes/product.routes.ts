2dbe537c73a0febc6317392f016a88f3bf318541
arasruislam <arasru01@gmail.com>
2025-07-08 05:25:39 +0600

    learning moduler system backend

--- fundamentals/express.js/y_express/src/routes/product.routes.ts
+++ fundamentals/express.js/y_express/src/routes/product.routes.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { handleProducts } from "../controller/product.controller";
 
 export const routeHandler = async (
   req: IncomingMessage,
@@ -8,5 +9,10 @@ export const routeHandler = async (
   if (url === "/" && req.method === "GET") {
     res.writeHead(200, { "content-type": "text/plain" });
     res.end("Welcome to node.js server");
+  } else if (url?.startsWith("/products")) {
+    await handleProducts(req, res);
+  } else {
+    res.writeHead(404);
+    res.end("Bhai to poth vul korchen!!!");
   }
 };
